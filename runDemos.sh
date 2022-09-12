bf9d3a69cb3ec2b9a2f426db9f884817578cb6f6
pgfrank1 <patrickfrank1995@gmail.com>
2022-09-12 16:35:12 -0500

    Completed ReadDemo

---
+++ runDemos.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+java -classpath lib/java112Demos.jar java112.demos.$1 $2 $3 $4 $5
--- src/java112/demos/ReadDemo.java
+++ src/java112/demos/ReadDemo.java
@@ -4,11 +4,32 @@ import java.io.*;
 
 public class ReadDemo {
     public static void main(String[] args) {
+        BufferedReader reader = null;
         try {
-        FileReader reader = new FileReader("input.txt");
+            // FileReader reader = new FileReader("input.txt");
+            // BufferedReader bufferedReader = new BufferedReader(reader);
+            reader = new BufferedReader(new FileReader("input.txt"));
+            while (reader.ready()) {
+                System.out.println(reader.readLine());
+            }
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.println("File not found");
             fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            System.out.println("File could not be read");
+            ioException.printStackTrace();
+        } catch (Exception exception) {
+            System.out.println("There was a problem.");
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                reader.close();
+                }
+            } catch (Exception exception) {
+                System.out.println("There was a problem closing the file");
+                exception.printStackTrace();
+            }
         }
     }
 
