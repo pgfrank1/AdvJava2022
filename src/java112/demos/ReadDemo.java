5208eba28d81f27c23cfb40cdb39180887572f5b
pgfrank1 <patrickfrank1995@gmail.com>
2022-09-12 16:05:50 -0500

    Read Demo with constructor and first exception handling

--- src/java112/demos/ReadDemo.java
+++ src/java112/demos/ReadDemo.java
@@ -1,6 +1,16 @@
 package java112.demos;
 
+import java.io.*;
+
 public class ReadDemo {
-    FileReader reader = new FileReader("input.txt");
+    public static void main(String[] args) {
+        try {
+        FileReader reader = new FileReader("input.txt");
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("File not found");
+            fileNotFoundException.printStackTrace();
+        }
+    }
+
     
 }
