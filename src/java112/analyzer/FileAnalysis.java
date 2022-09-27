e806f11ad1816f32ade686350b7624d7d1802472
pgfrank1 <patrickfrank1995@gmail.com>
2022-09-27 11:19:45 -0500

    Added comments to the FileAnalysis class

--- src/java112/analyzer/FileAnalysis.java
+++ src/java112/analyzer/FileAnalysis.java
@@ -3,21 +3,38 @@ package java112.analyzer;
 import java.io.*;
 
 
-
+/**
+ * This class recieves the user entered argument and ensures that only one
+ * argument was entered. It then feeds each line of the input file to the
+ * openInputFile and writeOutputFiles methods.
+ * 
+ * @author pgfrank
+ */
 public class FileAnalysis {
+    // Declare needed constants
     static final int COMMAND_ARGS = 1;
     static final String OUTPUT_PATH = "./output/";
 
-
+    // Initialize 2 classes
     private FileSummaryAnalyzer summaryAnalyzer;
     private DistinctTokensAnalyzer distinctAnalyzer;
 
+    /**
+     * This constructor instantiates the two analyzer variables, summaryAnalyzer
+     * and distinctAnalyzer
+     */
     public FileAnalysis() {
         summaryAnalyzer = new FileSummaryAnalyzer();
         distinctAnalyzer = new DistinctTokensAnalyzer();
     }
-    
 
+    /**
+     * This method recieves the user entered argument and determines if there
+     * was only one argument passed. If so, send the user's argument to the
+     * openInputFile and writeOutputFiles methods.
+     * 
+     * @param arguments user entered input file
+     */
     public void analyze(String[] arguments) {
         if (arguments.length != COMMAND_ARGS) {
             System.out.println("You have not entered the correct amount of"
@@ -28,6 +45,13 @@ public class FileAnalysis {
         }
     }
 
+    /**
+     * This class takes the user entered file, attempts to open the file
+     * specified then sends each line of the file to the passUserFile method.
+     * It also catches any Exception that is likely to occur.
+     * 
+     * @param userInputFile user entered input file
+     */
     public void openInputFile(String userInputFile) {
         try (BufferedReader input = new BufferedReader(new FileReader(
                 userInputFile))){
@@ -44,12 +68,23 @@ public class FileAnalysis {
             exception.printStackTrace();
         }
     }
-
+    /**
+     * This method passes the user entered file to each Analyzer class
+     * 
+     * @param userInputFile user entered input file
+     */
     public void passUserFile(String userInputFile) {
         distinctAnalyzer.processToken(userInputFile);
         summaryAnalyzer.processToken(userInputFile);
     }
 
+    /**
+     * This method calls upon the generateOutputFiles from both\
+     * distinctAnalyzer and summaryAnalyzer classes. It sends the user entered
+     * file and the desired output path to the methods.
+     * 
+     * @param userInputFile user entered input file
+     */
     public void writeOutputFiles(String userInputFile) {
         distinctAnalyzer.generateOutputFile(userInputFile, OUTPUT_PATH);
         summaryAnalyzer.generateOutputFile(userInputFile, OUTPUT_PATH);
