13cfcef2f13e2d110e63c89a69a2681d118d0300
pgfrank1 <patrickfrank1995@gmail.com>
2022-11-11 07:07:07 -0600

    TokenLengthsAnalyzer implements PropertiesLoader and Token Analyzer

--- src/java112/project3/TokenLengthsAnalyzer.java
+++ src/java112/project3/TokenLengthsAnalyzer.java
@@ -1,6 +1,7 @@
 package java112.project3;
 
 import java.util.*;
+import java.io.*;
 import java112.analyzer.*;
 import java112.utilities.PropertiesLoader;
 
@@ -10,7 +11,7 @@ public class TokenLengthsAnalyzer implements PropertiesLoader, TokenAnalyzer{
     private Properties properties;
 
     public TokenLengthsAnalyzer() {
-        properties = new Properties(loadProperties("/project2.properties"));
+        properties = new Properties(loadProperties("/analyzer.properties"));
     }
 
     public Map<Integer, Integer> getTokenLengths() {
@@ -19,14 +20,40 @@ public class TokenLengthsAnalyzer implements PropertiesLoader, TokenAnalyzer{
 
     @Override
     public void processToken(String token) {
-        // TODO Auto-generated method stub
         int tokenLength = token.length();
+        if (tokenLength == 0) {
+            return;
+        }
+        if (tokenLengths.containsKey(tokenLength)) {
+            tokenLengths.replace(tokenLength, tokenLengths.get(tokenLength) + 1);
+            return;
+        }
+        tokenLengths.putIfAbsent(tokenLength, 1);
+
         
     }
 
     @Override
     public void generateOutputFile(String inputFilePath) {
-        // TODO Auto-generated method stub
+    /**
+     * Attempt to create a file at the specifed output path
+     */
+        try(PrintWriter fileOutput = new PrintWriter(new BufferedWriter(
+                new FileWriter(properties.getProperty("output.directory")
+                        + properties.getProperty("output.file.token.lengths"))))) {
+                    for (String longUniqueTokens : largestTokens) {
+                        /**
+                         * Prints each token to the output file
+                         */
+                        fileOutput.println(longUniqueTokens);
+                    }
+        } catch (IOException ioException) {
+            System.out.println("There was an error outputting the file.");
+            ioException.printStackTrace();
+        } catch (Exception exception) {
+            System.out.println("There was an error...");
+            exception.printStackTrace();
+        }
         
     }
 }
