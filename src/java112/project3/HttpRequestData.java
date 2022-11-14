bb00488ae6666f6c5e1ffe8778dd0e5a161dff61
pgfrank1 <patrickfrank1995@gmail.com>
2022-11-13 19:50:55 -0600

    HttpRequestData and HttpRequestServlet have been created

---
+++ src/java112/project3/HttpRequestData.java
@@ -0,0 +1,15 @@
+package java112.project3;
+
+public class HttpRequestData {
+    private String requestComputer;
+    private String requestIPAddress;
+    private String requestHttpMethod;
+    private String requestURI;
+    private StringBuffer requestURL;
+    private String requestProtocol;
+    private String serverName;
+    private String serverPort;
+    private String currentLocale;
+    private String queryString;
+    //private
+}
