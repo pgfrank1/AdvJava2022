e343721533f5c467bbd44c4b878c2a5f884e4668
pgfrank1 <patrickfrank1995@gmail.com>
2022-11-20 19:38:26 -0600

    Unit 4 Lab 1-3 complete, screenshots and reflection complete

--- src/java112/project4/JDBCPreparedStatement.java
+++ src/java112/project4/JDBCPreparedStatement.java
@@ -1,5 +1,47 @@
 package java112.project4;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+
 public class JDBCPreparedStatement {
+
+    public static void main(String[] args) {
+        JDBCPreparedStatement run = new JDBCPreparedStatement();
+
+        run.runPreparedStatement();
+    }
+
+    public void runPreparedStatement() {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/student",
+                    "student", "student");
+
+            preparedStatement = connection.prepareStatement("INSERT INTO employees VALUES (null, ?, ?, ?, ?, ?, ?)");
+
+            preparedStatement.setString(1, "Stephanie");
+            preparedStatement.setString(2, "Frank");
+            preparedStatement.setString(3, "987-65-4321");
+            preparedStatement.setString(4, "HR");
+            preparedStatement.setString(5, "300");
+            preparedStatement.setString(6, "987-6543");
+
+            preparedStatement.executeUpdate();
+
+        } catch (ClassNotFoundException classNotFoundException) {
+            classNotFoundException.printStackTrace();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } catch (Exception exception) {
+            System.err.println("General Error");
+        }
+
+    }
 
 }
