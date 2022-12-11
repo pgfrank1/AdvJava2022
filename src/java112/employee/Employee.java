52eb5bdaf37175391dab1dec44cc8076dab45af1
pgfrank1 <patrickfrank1995@gmail.com>
2022-12-11 16:23:36 -0600

    feat(Project4): Adding new Employee class in the Project 4 package
    Created the Employee class for Project 4 and added it to the Project 4 package
    
    Author: pgfrank

---
+++ src/java112/employee/Employee.java
@@ -0,0 +1,140 @@
+package java112.employee;
+
+/**
+ * The type Employee.
+ */
+public class Employee {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+    private String department;
+    private String roomNumber;
+    private String phoneNumber;
+
+    /**
+     * Gets employee id.
+     *
+     * @return the employee id
+     */
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    /**
+     * Sets employee id.
+     *
+     * @param employeeId the employee id
+     */
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets social security number.
+     *
+     * @return the social security number
+     */
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    /**
+     * Sets social security number.
+     *
+     * @param socialSecurityNumber the social security number
+     */
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    /**
+     * Gets department.
+     *
+     * @return the department
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+    /**
+     * Sets department.
+     *
+     * @param department the department
+     */
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    /**
+     * Gets room number.
+     *
+     * @return the room number
+     */
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    /**
+     * Sets room number.
+     *
+     * @param roomNumber the room number
+     */
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    /**
+     * Gets phone number.
+     *
+     * @return the phone number
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * Sets phone number.
+     *
+     * @param phoneNumber the phone number
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
